276d973d4331a7a40919a020d3c20f86d5e05917
ABOBA123 <tarasovlev7@gmail.com>
2023-02-26 22:23:35 +0300

    asd

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
-function App() {
-  return <div className='App'></div>;
+import React, { useState } from 'react';
+
+function Chat() {
+  const [messages, setMessages] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const input = event.target.elements.message;
+    const newMessage = input.value;
+    setMessages([...messages, newMessage]);
+    input.value = '';
+  };
+
+  return (
+    <div className="chat">
+      <h1>React Chat</h1>
+      <div className="messages">
+        {messages.map((message, index) => (
+          <Message key={index} text={message} />
+        ))}
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="message" placeholder="Enter message" />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
 }
 
-export default App;
+function Message(props) {
+  return <div className="message">{props.text}</div>;
+}
+export default Chat
